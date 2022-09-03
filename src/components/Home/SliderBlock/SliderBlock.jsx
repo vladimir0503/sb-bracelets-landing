3940608fbd31f6a4ac28b5b1a6cf8265a69cf348
Vladimir <vovan0503@mail.ru>
2022-09-03 20:49:23 +0300

    Commit

--- src/components/Home/SliderBlock/SliderBlock.jsx
+++ src/components/Home/SliderBlock/SliderBlock.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import s from './SliderBlock.module.scss';
 
 const SliderBlock = () => {
+    const [screenHeight, setHeight] = React.useState();
 
     const navigate = useNavigate('');
 
@@ -11,8 +12,12 @@ const SliderBlock = () => {
         navigate('/catalog');
     };
 
+    React.useEffect(() => {
+        setHeight(window.screen.height);
+    }, []);
+
     return (
-        <div className={s.sliderBlock}>
+        <div style={{'height': `${screenHeight}px`}} className={s.sliderBlock}>
             <div className={s.sliderBlockContent}>
                 <h1>Реализуйте изделия для себя и<br></br> своих близких</h1>
                 <p>Добавте персональную гравировку: имена<br></br>

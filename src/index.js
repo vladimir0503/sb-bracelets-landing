27edb7e316da24ed95e1e0a1686168a6badba983
Vladimir <vovan0503@mail.ru>
2022-09-19 14:09:35 +0300

    Commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { hydrate, render } from 'react-dom';
 import './index.scss';
 import App from './App/App';
 import { store } from './App/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>
-);
+const rootElement = document.getElementById("root");
+
+const app = <Router>
+  <Provider store={store}>
+    <App />
+  </Provider>
+</Router>;
+
+if (rootElement.hasChildNodes()) {
+  hydrate(app, rootElement);
+} else {
+  render(app, rootElement);
+};
